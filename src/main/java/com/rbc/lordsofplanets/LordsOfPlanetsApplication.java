e758f3763235cfab1a62b9fa12af8439129aaa9a
frique92 <29735207+frique92@users.noreply.github.com>
2021-04-05 19:45:39 +0300

    Initial commit

---
+++ src/main/java/com/rbc/lordsofplanets/LordsOfPlanetsApplication.java
@@ -0,0 +1,13 @@
+package com.rbc.lordsofplanets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LordsOfPlanetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LordsOfPlanetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rbc/lordsofplanets/LordsOfPlanetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbc.lordsofplanets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LordsOfPlanetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
