9a1fa881d7474d2d6e57e7c8df2802fc4d980fea
frique92 <29735207+frique92@users.noreply.github.com>
2021-04-06 14:30:02 +0300

    add test for getPlanets()

--- src/test/java/com/rbc/lordsofplanets/LordsOfPlanetsApplicationTests.java
+++ src/test/java/com/rbc/lordsofplanets/LordsOfPlanetsApplicationTests.java
@@ -10,4 +10,6 @@ class LordsOfPlanetsApplicationTests {
 	void contextLoads() {
 	}
 
+
+
 }
--- src/test/java/com/rbc/lordsofplanets/controllers/PlanetControllerTest.java
+++ src/test/java/com/rbc/lordsofplanets/controllers/PlanetControllerTest.java
@@ -1,10 +1,23 @@
 package com.rbc.lordsofplanets.controllers;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.rbc.lordsofplanets.models.Lord;
+import com.rbc.lordsofplanets.models.Planet;
+import com.rbc.lordsofplanets.repositories.LordRepository;
+import com.rbc.lordsofplanets.repositories.PlanetRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.nullValue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,5 +28,53 @@ class PlanetControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private LordRepository lordRepository;
 
+    @Autowired
+    private PlanetRepository planetRepository;
+
+    @Test
+    void getPlanets() throws Exception{
+
+        MvcResult mvcResult = this.mockMvc.perform(get("/api/planets"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String contentAsString = mvcResult.getResponse().getContentAsString();
+
+        assertNotNull(contentAsString);
+
+        Planet planet = new Planet();
+        planet.setName("Earth");
+
+        planetRepository.save(planet);
+
+        mvcResult = this.mockMvc.perform(get("/api/planets"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+
+        contentAsString = mvcResult.getResponse().getContentAsString();
+
+        assertEquals(contentAsString, "[{\"id\":1,\"name\":\"Earth\",\"lord\":null}]");
+
+    }
+
+    @Test
+    void addPlanet() {
+    }
+
+    @Test
+    void getPlanet() {
+    }
+
+    @Test
+    void updateLordOfPlanet() {
+    }
+
+    @Test
+    void deletePlanet() {
+    }
 }
